027b34234b7e4751e2bf938852108d39e3cf4b0e
koleshchatov <koleshchatov@icloud.com>
2026-01-13 14:12:48 +0500

    Создаю структуру папок

---
+++ my-react-app/src/Routes/index.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Userfront } from "@userfront/react";
+import { SignupForm } from "@userfront/toolkit";
+
+function App() {
+  return (
+    <>
+      <div style={(width = 100)}>
+        <div style={{ float: left, width: 80 }}>left</div>
+        <div style={{ float: right }}>right</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
