ad3269c22f22cc044402ebb5ab8b4db3990d7742
koleshchatov <koleshchatov@icloud.com>
2026-04-10 14:19:33 +0500

    add deploy

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: /github.com/koleshchatov/newReal,
+  base: /github.com/koleshchatov/newReal/my-react-app,
   plugins: [react()],
 })
