ea0ee2d9e9f5b019460c080494ff0ee83c253fa3
koleshchatov <koleshchatov@icloud.com>
2026-04-14 12:42:00 +0500

    localeStorage

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./Components/Routes";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./Auth/store";
+import { store } from "./Auth/store.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
